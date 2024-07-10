7dee9a3e3011ee6e0387e85b1d244951ea6c0046
kuldeep singh mehra <kuldeepm272@gmail.com>
2024-07-10 09:20:32 +0530

    added gameStore

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="bg-primary text-white py-10">
+        <div className="container text-center">
+          <h1 className="text-3xl font-bold">Let's Play the Game</h1>
+          <hr className="w-[150px] mx-auto my-4" />
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
